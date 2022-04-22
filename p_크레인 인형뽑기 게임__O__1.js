f8509173da48b7808b1e109062514fb53e83d1f4
dahin <72854680+kimdahuin12@users.noreply.github.com>
2022-04-23 00:10:00 +0900

    크레인 인형뽑기 게임

---
+++ p_크레인 인형뽑기 게임__O__1.js
@@ -0,0 +1,22 @@
+function solution(board, moves) {
+    var answer = 0;
+    var basket = [];
+    var tmpBoard = new Array(board.length);
+    
+    for(i = 0; i < board.length; i++){
+        tmpBoard[i] = [];
+        for(j = board.length-1; j >= 0; j--){
+            if(board[j][i]!==0){ tmpBoard[i].push(board[j][i]); }
+        }
+    }
+    
+    for(i = 0; i < moves.length; i++){
+       var doll = tmpBoard[moves[i]-1].pop();
+        if(doll !== undefined){
+            if(basket.length===0||basket[basket.length-1]!==doll){ basket.push(doll); }
+			else{ basket.pop(); answer+=2; }
+        }
+    }
+    
+    return answer;
+}
